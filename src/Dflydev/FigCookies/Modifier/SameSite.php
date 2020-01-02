7db398753cc673960dbd65b6e1135ea194bf0462
Sean Hannan <seanphannan@gmail.com>
2020-01-01 22:00:13 -0500

    Added support for SameSite=None. Updated tests.

--- src/Dflydev/FigCookies/Modifier/SameSite.php
+++ src/Dflydev/FigCookies/Modifier/SameSite.php
@@ -9,22 +9,34 @@ use function strtolower;
 
 final class SameSite
 {
-    /** @var bool */
-    private $strict;
+    /**
+     * The possible string values of the SameSite setting
+     */
+    private const STRICT = 'Strict';
+    private const LAX    = 'Lax';
+    private const NONE   = 'None';
 
-    private function __construct(bool $strict)
+    /** @var string */
+    private $value;
+
+    private function __construct(string $value)
     {
-        $this->strict = $strict;
+        $this->value = $value;
     }
 
     public static function strict() : self
     {
-        return new self(true);
+        return new self(self::STRICT);
     }
 
     public static function lax() : self
     {
-        return new self(false);
+        return new self(self::LAX);
+    }
+
+    public static function none() : self
+    {
+        return new self(self::NONE);
     }
 
     /**
@@ -42,14 +54,18 @@ final class SameSite
             return self::lax();
         }
 
+        if ($lowerCaseSite === 'none') {
+            return self::none();
+        }
+
         throw new \InvalidArgumentException(sprintf(
-            'Expected modifier value to be either "strict" or "lax", "%s" given',
+            'Expected modifier value to be either "strict", "lax", or "none", "%s" given',
             $sameSite
         ));
     }
 
     public function asString() : string
     {
-        return 'SameSite=' . ($this->strict ? 'Strict' : 'Lax');
+        return 'SameSite=' . $this->value;
     }
 }
